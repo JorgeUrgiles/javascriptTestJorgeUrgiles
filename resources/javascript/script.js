3ba97c659e2c2eb0c931e0634c61c7b46939591a
JorgUrgiles <jorge.urgilesc@gmail.com>
2022-11-11 10:23:19 -0500

    cambios

---
+++ resources/javascript/script.js
@@ -0,0 +1,14 @@
+console.log("BIENVENIDO A LA EVALUACION DE JAVASCRIPT!!!")
+function mainFuction(){
+    printText()
+
+}
+function printText(){
+    let value2=document.getElementById("text1")
+    let value1=document.getElementById("text1").value
+    if(value1==''){alert("Ups! Nada que mostrar")}else{alert(value1)
+        
+    }
+    
+    
+}  
